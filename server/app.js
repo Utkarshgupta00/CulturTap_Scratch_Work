551bd433987c10be1746ed4d75b0f31b19dc7cf7
Utkarsh <Aishwary.009.shrivastava@culturtap.com>
2023-11-01 04:53:35 +0530

    changes in feature

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mainRoute = require('./routes/mainRoute.js');
-const port = 8080;
+const port = 4040;
 require('./db/conn.js');
 const UserData = require('./db/model/UserData.js');
 const cors = require('cors');
